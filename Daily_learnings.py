ab6b2da1ee51c26bd4b741571b6acb7094144ec4
Riturajlodhi <lodhirituraj1234@gmail.com>
2026-01-27 15:14:59 +0530

    update

--- Daily_learnings.py
+++ Daily_learnings.py
@@ -6,6 +6,37 @@ name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 print(name,age) """
 
+# sorting Lists
+""" def myfunc(n):
+  return abs(n - 50)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(key = myfunc)
+print(thislist)
+
+def func(n):
+  return abs(n - 30)
+
+thislist = [90, 45, 84, 73, 28]
+thislist.sort(key = func)
+print(thislist)
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort(key=str.upper)
+print(thislist)
+"""
  # copying lists using copy() method
 
 thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+# using list method
+my_list  = list(thislist)
+print(my_list)
+
+#using : operator
+Mylist = thislist[:]
+print(Mylist)
+
+#Joining lists
