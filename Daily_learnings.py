49cceac954310530c2435764e516b155b6caed10
Riturajlodhi <lodhirituraj1234@gmail.com>
2026-01-27 14:05:45 +0530

    Initial commit

---
+++ Daily_learnings.py
@@ -0,0 +1,11 @@
+# print("Hello everyone we are learning Python")
+# num = 10
+# print(num)
+"""
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print(name,age) """
+
+ # copying lists using copy() method
+
+thislist = ["apple", "banana", "cherry"]
